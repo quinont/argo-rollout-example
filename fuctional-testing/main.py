1ce78f53c643dbc74e6487cb9e371d20dc82afa3
Juan Esteban Colcombet <quinont@gmail.com>
2023-12-17 11:30:05 -0300

    Creando el ejecutador de los test funcionales

---
+++ fuctional-testing/main.py
@@ -0,0 +1,67 @@
+import subprocess
+import os
+from git import Repo
+
+
+def clone_repo(repo_url, commit_hash):
+    """
+    Clonea un repo de git en un commit especial.
+
+    Args:
+        repo_url: La URL del repo de git.
+        commit_hash: El hash del commit especial.
+
+    Returns:
+        El directorio del repo clonado.
+    """
+    repo_dir = os.path.join(".", os.path.basename(repo_url))
+    if os.path.exists(repo_dir):
+        subprocess.run(["rm", "-rf", repo_dir])
+    try:
+        repo = Repo.clone_from(repo_url, 'argo-rollout-example')
+
+        # Cambiar al commit específico
+        repo.git.reset('--hard', commit_hash)
+
+        print("Clonado y restablecido con éxito.")
+    except Exception as e:
+        raise Exception(f"Error al clonar o restablecer el repositorio: {e}")
+    return repo_dir
+
+
+def run_tests(repo_dir, folder):
+    """
+    Ejecuta los test unitarios de un repo.
+
+    Args:
+        repo_dir: El directorio del repo.
+
+    Returns:
+        El exit code de los test.
+    """
+    test_files = []
+    for f in os.listdir(os.path.join(repo_dir, folder)):
+        if os.path.isfile(os.path.join(repo_dir, folder, f)):
+            if f.endswith(".py"):
+                test_files.append(os.path.join(repo_dir, folder, f))
+    print(f"Archivos encontrados: {test_files}")
+    return subprocess.call(["pytest", *test_files])
+
+
+if __name__ == "__main__":
+    repo_url = os.getenv("REPO_URL")
+    if repo_url is None:
+        raise Exception("No se encontro variable de entorno REPO_URL")
+
+    commit_hash = os.getenv("COMMIT_HASH")
+    if commit_hash is None:
+        raise Exception("No se encontro variable de entorno COMMIT_HASH")
+
+    folder_test = os.getenv("FOLDER_TEST")
+    if folder_test is None:
+        raise Exception("No se encontro variable de entorno FOLDER_TEST")
+
+    repo_dir_clone = clone_repo(repo_url, commit_hash)
+    exit_code = run_tests(repo_dir_clone, folder_test)
+    if exit_code != 0:
+        raise Exception("Los test fallaron")
